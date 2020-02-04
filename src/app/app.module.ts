dba17254111ee3106362da1df3c961d257b59da1
Vaughan <vaughan.webber@iwyze.co.za>
2020-02-04 10:02:48 +0200

    lazy loading working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './components/home/home.module';
+import { ContactModule } from './components/contact/contact.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    ContactModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
